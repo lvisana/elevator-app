40f584afe253306d9805ae8e2f235201c484533e
lvisana <lsmm2705@gmail.com>
2024-07-01 14:51:20 -0400

    Building layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Elevator App</h1>
+  <div class="building-area-container">
+    <div class="add">
+      <img
+        src="https://www.svgrepo.com/show/2087/plus.svg"
+        width="30"
+        height="30"
+      />
+    </div>
+    <BuildingArea />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BuildingArea from "./components/BuildingArea.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    BuildingArea,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
